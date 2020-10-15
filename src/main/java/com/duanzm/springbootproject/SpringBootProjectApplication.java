3726296c11c140385491867981c07581938cbe6f
zhimin <dzm0305@163.com>
2020-10-15 11:15:02 +0800

    Initial commit

---
+++ src/main/java/com/duanzm/springbootproject/SpringBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.duanzm.springbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootProjectApplication {
+
+    //将springboot应用启动
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootProjectApplication.class, args);
+    }
+
+}
