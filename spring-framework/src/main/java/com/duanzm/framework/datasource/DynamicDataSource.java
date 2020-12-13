2a271fb2f8c786b3d0ad39cbd1b77dd51378682b
zhimin <dzm0305@163.com>
2020-12-13 16:44:13 +0800

    笔记

--- spring-framework/src/main/java/com/duanzm/framework/datasource/DynamicDataSource.java
+++ spring-framework/src/main/java/com/duanzm/framework/datasource/DynamicDataSource.java
@@ -1,5 +1,7 @@
 package com.duanzm.framework.datasource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.jdbc.datasource.lookup.AbstractRoutingDataSource;
 
 import javax.sql.DataSource;
@@ -7,8 +9,27 @@ import java.util.Map;
 
 /**
  * 动态数据源
+ *
+ * 实现逻辑：
+ * 1、定义DynamicDataSource类继承抽象类AbstractRoutingDataSource，并实现了determineCurrentLookupKey()方法。
+ * 2、把配置的多个数据源会放在AbstractRoutingDataSource的 targetDataSources 和 defaultTargetDataSource 中，
+ *    然后通过afterPropertiesSet()方法将数据源分别进行复制到resolvedDataSources和resolvedDefaultDataSource中。
+ * 3、调用AbstractRoutingDataSource的 getConnection() 的方法的时候，先调用determineTargetDataSource()方法返回DataSource在进行getConnection()
  */
 public class DynamicDataSource extends AbstractRoutingDataSource {
+    public static final Logger log = LoggerFactory.getLogger(DynamicDataSource.class);
+
+    /**
+     * 使用ThreadLocal维护变量，ThreadLocal为每个使用该变量的线程提供独立的变量副本，
+     * 所以每一个线程都可以独立地改变自己的副本，而不会影响其它线程所对应的副本。
+     */
+    private static final ThreadLocal<String> CONTEXT_HOLDER = new ThreadLocal<>();
+
+    /**
+     *
+     * @param defaultTargetDataSource 默认数据源
+     * @param targetDataSources       目标数据源
+     */
     public DynamicDataSource(DataSource defaultTargetDataSource, Map<Object, Object> targetDataSources) {
         super.setDefaultTargetDataSource(defaultTargetDataSource);
         super.setTargetDataSources(targetDataSources);
@@ -17,6 +38,28 @@ public class DynamicDataSource extends AbstractRoutingDataSource {
 
     @Override
     protected Object determineCurrentLookupKey() {
-        return DynamicDataSourceContextHolder.getDataSourceType();
+        return getDataSourceType();
+    }
+
+    /**
+     * 获得数据源的变量
+     */
+    public static String getDataSourceType() {
+        return CONTEXT_HOLDER.get();
+    }
+
+    /**
+     * 设置数据源的变量
+     */
+    public static void setDataSourceType(String dsType) {
+        log.info("切换到{}数据源", dsType);
+        CONTEXT_HOLDER.set(dsType);
+    }
+
+    /**
+     * 清空数据源变量
+     */
+    public static void clearDataSourceType() {
+        CONTEXT_HOLDER.remove();
     }
 }
---
+++ spring-framework/src/main/java/com/duanzm/framework/utils/StringUtils.java
@@ -0,0 +1,29 @@
+package com.duanzm.framework.utils;
+
+/**
+ * 字符串工具类
+ *
+ * @author ruoyi
+ */
+public class StringUtils extends org.apache.commons.lang3.StringUtils {
+    /**
+     * * 判断一个对象是否为空
+     *
+     * @param object Object
+     * @return true：为空 false：非空
+     */
+    public static boolean isNull(Object object) {
+        return object == null;
+    }
+
+    /**
+     * * 判断一个对象是否非空
+     *
+     * @param object Object
+     * @return true：非空 false：空
+     */
+    public static boolean isNotNull(Object object) {
+        return !isNull(object);
+    }
+
+}
